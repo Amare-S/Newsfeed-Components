6000309027dc60ebead7b8673ae7da9f6b3c767a
Amare Shakur <amare-shakur@lambdastudents.com>
2021-01-13 19:22:31 -0500

    completed all steps

--- components/Article.js
+++ components/Article.js
@@ -93,7 +93,7 @@ const data = [
   Step 1: Write a component called 'articleMaker' to create an article.
   Your component is a function that takes an article object as its only argument,
   and returns a DOM node looking like the one below:
-
+ 
   <div class="article">
     <h2>{title of the article}</h2>
     <p class="date">{date of the article}</p>
@@ -106,6 +106,9 @@ const data = [
   Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
   This listener should toggle the class 'article-open' on div.article.
 
+  articleSpan.addEventListener('click', (event) => {
+  articleSpan.classList.toggle('article-open')
+})
   Step 3: Don't forget to return something from your function!
 
   Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
